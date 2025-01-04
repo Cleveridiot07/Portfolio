66c9ed1cf19d1360d6a0a1c174dea05e6971d00f
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-01-04 17:28:39 +0530

    updated projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,23 +4,24 @@ import { Project } from "../../interfaces";
 
 
 const projects:Project[] = [
+
   {
-    title: "Note Forge",
+    title: "VocaScribe",
     description:
-      "NoteForge is a MERN stack-based notes application that offers secure JWT-based authentication, customizable note colors, and a sleek interface powered by Vite and Tailwind CSS. Deployed on Vercel, it ensures a seamless and responsive user experience.",
-    techStack: ["ReactJS", "NodeJS", "TailwindCSS", "ExpressJS"],
-    lastUpdated: "2023-01-01",
-    imageUrl: "./Projects/NoteForge.jpeg",
-    githubUrl: "https://github.com/Cleveridiot07/NoteForge",
-    visitUrl: "https://noteforge-frontend.onrender.com/",
-    imageBg: "bg-amber-950",
+      "A seamless speech-to-text transcriber that transforms your voice into accurate, real-time text with ease and efficiency.",
+    techStack: ["ReactJS", "Typescript", "AWS","Microsoft Azure"],
+    lastUpdated: "2025-01-03", 
+    imageUrl: "./Projects/Ai.jpeg",
+    githubUrl: "https://github.com/Cleveridiot07/Vocascribe-Text-to-Speech-convertor",
+    visitUrl: "https://vocascribe.netlify.app/",
+    imageBg: "bg-white",
   },
   {
     title: "Brain Tumor Classification",
     description:
       "This project is a Brain Tumor Detection API built with FastAPI and TensorFlow, allowing users to upload MRI images and predict tumor presence using a pre-trained CNN model. It is optimized for serverless deployment on AWS Lambda.",
     techStack: ["Python", "FastAPI", "AWS"],
-    lastUpdated: "2023-12-28", 
+    lastUpdated: "2024-10-14", 
     imageUrl: "./Projects/BrainTumorClassification.webp",
     githubUrl: "https://github.com/Cleveridiot07/BrainTumorClassificationUsingOpenCV",
     visitUrl: "https://github.com/Cleveridiot07/BrainTumorClassificationUsingOpenCV",
@@ -31,7 +32,7 @@ const projects:Project[] = [
     description:
       "A scalable, cloud-based IDE supporting real-time collaboration for developers. Designed for seamless remote development.",
     techStack: ["ReactJS", "ExpressJS", "Docker", "Firebase", "TailwindCSS"],
-    lastUpdated: "2023-12-25",
+    lastUpdated: "2024-02-24",
     imageUrl: "./Projects/CloudIDE.jpeg",
     githubUrl: "https://github.com/Cleveridiot07/DevIDE",
     visitUrl: "https://github.com/Cleveridiot07/DevIDE",
@@ -53,11 +54,22 @@ const projects:Project[] = [
     description:
       "A visually stunning MERN stack portfolio featuring a sleek design, responsive interface, and a showcase of projects, skills, and achievements.",
     techStack: ["ReactJS", "NodeJS", "TailwindCSS", "ExpressJS"],
-    lastUpdated: "2023-01-01",
+    lastUpdated: "2025-01-01",
     imageUrl: "./Projects/Portfolio.jpg",
     githubUrl: "https://github.com/Cleveridiot07/Portfolio",
-    visitUrl: "https://github.com/Cleveridiot07/Portfolio",
+    visitUrl: "https://cleveridiot07.netlify.app/",
     imageBg: "bg-gray-950",
+  },
+  {
+    title: "Note Forge",
+    description:
+      "NoteForge is a MERN stack-based notes application that offers secure JWT-based authentication, customizable note colors, and a sleek interface powered by Vite and Tailwind CSS. Deployed on Vercel, it ensures a seamless and responsive user experience.",
+    techStack: ["ReactJS", "NodeJS", "TailwindCSS", "ExpressJS"],
+    lastUpdated: "2023-08-01",
+    imageUrl: "./Projects/NoteForge.jpeg",
+    githubUrl: "https://github.com/Cleveridiot07/NoteForge",
+    visitUrl: "https://noteforge-frontend.onrender.com/",
+    imageBg: "bg-amber-950",
   },
 ];
 
